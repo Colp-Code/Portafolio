b39c2aba9cc8b6d131645e1d8fd5a85c25f223f9
IchiCD <doncolpcode@gmail.com>
2025-11-12 15:14:45 -0500

    v0.2.0

--- my-vue-app/src/Pages/Home/home.jsx
+++ my-vue-app/src/Pages/Home/home.jsx
@@ -1,6 +1,14 @@
+import { Github, Linkedin } from 'lucide-react';
+import './home.css'
 const Home = () =>{
     return(
         <>
+            <img src="https://i.ibb.co/tMzqHWnP/image-1.png" alt="image-1" border="0"></img>
+            <h1>¿Quien soy?</h1>
+            <h3>Hola, soy un apasionado del desarrollo web y la programación. Me encanta aprender nuevas tecnologías y transformar ideas en proyectos reales. Trabajo con Python, React.js y SQL, creando aplicaciones web funcionales y bien estructuradas.</h3>
+            <Github />
+            <Linkedin />
+            <p>𝕏</p>
             <p>hola</p>
         </>
     )
